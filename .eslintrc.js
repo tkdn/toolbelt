f92280dccd217d8c8734d86a95a5b8e2ef37b076
Satoshi Takeda <tkdnation@gmail.com>
2017-12-27 05:32:56 +0900

    :heavy_plus_sign: Add ESLint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'standard',
+  plugins: ['prettier', 'standard', 'node', 'promise', 'import'],
+  rules: {
+    'prettier/prettier': ['error', require('./package.json').prettier]
+  },
+  env: {
+    node: true,
+    es6: true
+  }
+}
