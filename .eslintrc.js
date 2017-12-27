f8d437fd6b6723056a7fcf1df5a4df67384ccdf4
tkdn <tkdnation@gmail.com>
2017-12-27 15:07:01 +0900

    :rotating_light: mod: eslint ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017
+  },
   parser: 'babel-eslint',
   extends: 'standard',
   plugins: ['prettier', 'standard', 'node', 'promise', 'import'],
   rules: {
-    'prettier/prettier': ['error', require('./package.json').prettier]
+    'prettier/prettier': ['error', require('./package.json').prettier],
+    // prefer to prettier
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
   },
   env: {
     node: true,
